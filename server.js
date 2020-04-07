13e3fe442f15f45bfe3c8a4ccffa75f7409a9e13
Abraham Serafino <ASerafino@werner.com>
2020-04-07 10:47:40 -0500

    Add React

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 const { port } = config
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.listen(port, () =>
   console.log(`Listening on :${port}`)
